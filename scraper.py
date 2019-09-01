8094c845d442fb63949ee370ebd79effe207b136
s4kis <gsakel5@gmail.com>
2019-09-01 15:27:03 +0300

    it works for the title class

--- scraper.py
+++ scraper.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-URL = "https://www.amazon.com/Enhanced-Splashproof-Portable-Bluetooth-Radiator/dp/B010OYASRG/ref=sxin_2_ac_d_pm?ac_md=2-0-VW5kZXIgJDMw-ac_d_pm&keywords=bluetooth+speaker&pd_rd_i=B010OYASRG&pd_rd_r=7e9d0246-6e5d-4b6f-a9bb-fa4c4d1aca95&pd_rd_w=EBj63&pd_rd_wg=x2f2N&pf_rd_p=eeff02d5-070a-45ea-a79e-d591974b877e&pf_rd_r=3ESD78PWJSG42ZBPTEYD&psc=1&qid=1567336060&s=gateway"
-#URL is basically our amazon item
+# URL is basically our amazon item
+URL = "https://www.amazon.co.uk/Bluetooth-Anker-SoundCore-Portable-Playtime-Black/dp/B01HTH3C8S/ref=sr_1_3?keywords=bluetooth+speaker&qid=1567340467&s=gateway&sr=8-3"
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'} #basically our browser info, my user agent on google should give me info for that
 page = requests.get(URL, headers=headers) #requests the page with the URL and header variable given at the top
 soup = BeautifulSoup(page.content, 'html.parser') #soup can give us the entire code from a website, just like view code source from f12
 #print(soup.prettify()) #prints the entire code from a website
-title = soup.find(id="productTitle")
-print(title)
+title = soup.find(id="productTitle").get_text()
+print(title.strip())
